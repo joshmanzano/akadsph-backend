5f02de935e9c3ae180572869847ccec4fe58e587
joshmanzano <josh_manzano@dlsu.edu.ph>
2021-08-07 00:18:52 +0800

    feat: backendsockets.routing

--- backend/routing.py
+++ backend/routing.py
@@ -3,7 +3,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import backendsockets
+import backendsockets.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
