55216ad2522bab3f720a863d6a35dd13832e623e
joshmanzano <josh_manzano@dlsu.edu.ph>
2021-08-12 18:57:01 +0800

    feat: model change in shop item

--- core/models.py
+++ core/models.py
@@ -237,7 +237,7 @@ class TutorPayout(models.Model):
 	isPaid = models.BooleanField(default = False)
 
 class ShopItem(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 	amount = models.IntegerField()
 	name = models.CharField(max_length=1000)
 	credits = models.IntegerField()
